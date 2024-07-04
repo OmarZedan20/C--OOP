9468772bb62036b6ee830e4f041f2318978e8903
omarzedan20 <omarzcb@gmail.com>
2024-07-04 22:06:52 +0300

    solve Q1

--- C# Assignment 1(OOP)/Program.cs
+++ C# Assignment 1(OOP)/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-           
-        }
+        #region Q1
+        //public enum WeekDays
+        //{
+        //    Monday,
+        //    Tuesday,
+        //    Wednesday,
+        //    Thursday,
+        //    Friday,
+        //    Saturday,
+        //    Sunday
+        //}
+        //static void Main(string[] args)
+        //{
+        //    // WeekDays days = WeekDays.Monday;
+
+        //    foreach (var day in Enum.GetValues(typeof(WeekDays)))
+        //    {
+        //        Console.WriteLine($"{day} = {(int)day} ");
+
+        //    }
+
+        //}
+
+        #endregion
+
     }
 }
